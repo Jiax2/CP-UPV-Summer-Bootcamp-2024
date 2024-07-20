fcaa297a2f60dd8e8154df2297ac95675bdd8b4c
Jiax2 <61508915+Jiax2@users.noreply.github.com>
2024-07-20 21:37:55 +0300

    Add files via upload

---
+++ day1/d.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    string s; 
+    getline(cin, s); 
+    transform(s.begin(), s.end(), s.begin(), ::tolower); 
+    cout<< s<< endl; 
+    transform(s.begin(), s.end(), s.begin(), ::toupper); 
+    cout<<s<<endl; 
+    return 0; 
+}
---
+++ day1/e.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    long long l1, l2; 
+    cin>>l1>> l2;  
+    cout<< l2<< endl; 
+    return 0; 
+}
