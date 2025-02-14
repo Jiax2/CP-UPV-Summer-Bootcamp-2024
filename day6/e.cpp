ab4dda48e6766dafe691d8049adbd1caa7675544
Jiax2 <jiajiali0208@gmail.com>
2025-02-14 09:36:13 +0100

    day 6

---
+++ day6/e.cpp
@@ -0,0 +1,13 @@
+int main(){
+
+}
+int n; 
+void dfs(){
+    //array visitados 
+    vis[][]
+    //caso base -> encuentra la c
+    if(m[x][y]== "C") return dist; 
+    //si llega al punto -> return
+    //caso general 
+    if(pos == no visitada &&  y + 1<n ) //buscamos 
+}
---
+++ template.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define Loop(it, x) for(int it= 0; it<x; it++)
+//DEBUG #define d(x) cerr << #x << "=" << x <<endl 
+
+int main(){
+    
+}
