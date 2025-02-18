d54aa344ba4666655511a89bf01b7fdc12438e13
Jiajia Li <61508915+Jiax2@users.noreply.github.com>
2025-02-18 12:48:26 +0100

    Update e.cpp

--- day6/e.cpp
+++ day6/e.cpp
@@ -1,13 +1,47 @@
-int main(){
-
+#include<bits/stdc++.h>
+using namespace std; 
+#define Loop(it, x) for(int it= 0; it<x; it++)
+//DEBUG #define d(x) cerr << #x << "=" << x <<endl 
+void DFS(int row,int col, vector<vector<char>> &adj, vector<vector<bool>> &visited, int count, int s){
+    
+    //H: limits 
+    if(row<0 || row>= s || col <0 ||col>=s || visited[row][col]){
+        return; 
+    }
+    
+    visited[row][col] = true; 
+    
+    if(adj[row][col] == 'C'){
+        cout<<count;
+        return; 
+    }else{
+        if(adj[row][col] == '0'|| adj[row][col]== 'R'){
+            DFS(row+1, col, adj, visited, count +1, s);
+            DFS(row, col+1, adj, visited, count +1, s); 
+            DFS(row, col-1, adj, visited, count+1, s); 
+        }
+    }
 }
-int n; 
-void dfs(){
-    //array visitados 
-    vis[][]
-    //caso base -> encuentra la c
-    if(m[x][y]== "C") return dist; 
-    //si llega al punto -> return
-    //caso general 
-    if(pos == no visitada &&  y + 1<n ) //buscamos 
+int main(){
+    int n; 
+    cin>>n; 
+    
+    vector<vector<char>> vect(n, vector<char>(n)); 
+    vector<vector<bool>> visit(n, vector<bool>(n,false)); 
+    int initR = 0, initC = 0; 
+    Loop(i, n){
+        Loop(j, n){
+            cin>>vect[i][j];
+            if(vect[i][j] == 'R'){
+                initR = i; 
+                initC = j; 
+            }
+            if(vect[i][j] == '.'){
+                visit[i][j] = true; 
+            }
+        }
+    }
+    
+    DFS(initR, initC, vect, visit, 0, n); 
+    return 0; 
 }
